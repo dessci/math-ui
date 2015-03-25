fe4d4e5cdee4041e662f18c1fcdb5c784108084a
Jan Marthedal Rasmussen <jan@janmr.com>
2015-03-25 14:34:36 +0100

    Paper bootswatch theme

--- libs/bootstrap/bootstrap.js
+++ libs/bootstrap/bootstrap.js
@@ -5,8 +5,8 @@
  */
 
 /*!
- * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=9b44b61d5b4b9770ab34)
- * Config saved to config.json and https://gist.github.com/9b44b61d5b4b9770ab34
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=12341dfe573cc53464ef)
+ * Config saved to config.json and https://gist.github.com/12341dfe573cc53464ef
  */
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
